ac21a90fba0194bca3578fc2a7e516d16adda903
Saumay Agrawal <saumay.agrawal@gmail.com>
2023-11-26 18:15:48 +0530

    add new example lottery

---
+++ lottery/src/Lottery.test.ts
@@ -0,0 +1,7 @@
+// import { Lottery } from './Lottery';
+
+describe('Lottery.js', () => {
+  describe('Lottery()', () => {
+    it.todo('should be correct');
+  });
+});
---
+++ lottery/src/index.ts
@@ -0,0 +1,3 @@
+import { Lottery } from './Lottery.js';
+
+export { Lottery };
